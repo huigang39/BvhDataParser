60a00713c10779af9eb95175864d7b373e169799
huigang39 <huigang39@outlook.com>
2021-10-01 20:12:10 +0800

    all file

---
+++ build/CMakeFiles/BvhDataParser.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "BvhDataParser.exe"
+  "BvhDataParser.exe.manifest"
+  "BvhDataParser.pdb"
+  "CMakeFiles/BvhDataParser.dir/src/main.cpp.obj"
+  "CMakeFiles/BvhDataParser.dir/src/main.cpp.obj.d"
+  "CMakeFiles/BvhDataParser.dir/src/readBvh.cpp.obj"
+  "CMakeFiles/BvhDataParser.dir/src/readBvh.cpp.obj.d"
+  "libBvhDataParser.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BvhDataParser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
